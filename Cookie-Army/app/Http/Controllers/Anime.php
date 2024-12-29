af7a751bb9200c8acdfaa4516fbc70fd818f17e4
DickyDD6 <dicky.223040150@mail.unpas.ac.id>
2024-12-29 23:43:05 +0700

    Fetch API Anime

---
+++ Cookie-Army/app/Http/Controllers/Anime.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+class Anime extends Controller
+{
+    public function index(){
+        $anime = Http::get('https://')["message"];
+        response()->json([
+            "data" => $anime,
+            "status_code" => 200
+          ], 200);
+    }
+}
