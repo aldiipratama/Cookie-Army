e1c40d8240e08a2b2adf2b3a2bf3f20703b85849
IlmanHanifa <ilmanhanifa9@gmail.com>
2025-01-02 22:39:49 +0700

    createStory

--- Cookie-Army/app/Http/Controllers/StoryController.php
+++ Cookie-Army/app/Http/Controllers/StoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Story;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class StoryController extends Controller
 {
@@ -20,10 +21,27 @@ class StoryController extends Controller
             ], 404);
         }
 
-
         return response()->json([
             "data" => $story,
             "status_code" => 200
         ], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validated();
+
+        if ($request->hasFile('image')) {
+            $imageName = time() . '.' . $request->image->extension();
+            $request->image->move(public_path('images'), $imageName);
+        }
+
+        $post = Story::create([
+            'caption' => $request->caption,
+            'image' => $imageName,
+            'user_id' => Auth::user()->id,
+        ]);
+
+        return response()->json(['message' => 'Post created successfully'], 201);
     }
 }
