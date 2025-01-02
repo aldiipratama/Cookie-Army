80d3aaa3bf8de7272ad4eb9615df687130a2b9bd
IlmanHanifa <ilmanhanifa9@gmail.com>
2025-01-02 22:30:35 +0700

    ShowAllData StoryController

--- Cookie-Army/app/Http/Controllers/StoryController.php
+++ Cookie-Army/app/Http/Controllers/StoryController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Story;
 use Illuminate\Http\Request;
 
 class StoryController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $story = Story::all();
+
+        if (!$story) {
+            return response()->json([
+                "error" => [
+                    "message" => "Data All User in Database Not Found !",
+                    "status_code" => 404
+                ]
+            ], 404);
+        }
+
+
+        return response()->json([
+            "data" => $story,
+            "status_code" => 200
+        ], 200);
+    }
 }
